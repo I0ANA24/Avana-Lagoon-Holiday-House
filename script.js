60932a58325c43c509bb46ee4c6ffbe02d8a198d
I0ANA24 <75781040+I0ANA24@users.noreply.github.com>
2024-09-01 13:37:22 +0300

    mobile version css & js

---
+++ script.js
@@ -0,0 +1,57 @@
+let hamburger = document.getElementById('menu-icon');
+let close = document.getElementById('menu-icon-close');
+let menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.style.display = "none";
+    close.style.display = "block";
+    menu.style.display = "flex";
+
+    document.addEventListener('click', (event) => {
+        if(close.style.display === "block" && !menu.contains(event.target) && !hamburger.contains(event.target)) {
+            close.style.display = "none";
+            hamburger.style.display = "block";
+            menu.style.display = "none";
+            document.body.style.overflow = "scroll";
+
+            if(event.target.tagName === 'A') {
+                event.preventDefault();
+            }
+        }
+    });
+});
+
+close.addEventListener('click', () => {
+    close.style.display = "none";
+    hamburger.style.display = "block";
+    menu.style.display = "none";
+    document.body.style.overflow = "scroll";
+});
+
+let startX = 0;
+let endX = 0;
+
+document.addEventListener('touchstart', function(e) {
+    startX = e.touches[0].clientX;
+});
+
+document.addEventListener('touchmove', function(e) {
+    endX = e.touches[0].clientX;
+});
+
+document.addEventListener('touchend', function(e) {
+    let deltaX = endX - startX;
+
+    if (deltaX > 100) { // swipe dreapta
+        hamburger.style.display = "none";
+        close.style.display = "block";
+        menu.style.display = "flex";
+        document.body.style.overflow = "hidden";
+    } else if (deltaX < -100) { // swipe stânga
+        close.style.display = "none";
+        hamburger.style.display = "block";
+        menu.style.display = "none";
+        document.body.style.overflow = "scroll";
+
+    }
+});
